64878aa5a5b0f7cebc0e2c67671267f4a03fa5d7
Hemanth V. Alluri <hdrive1999@gmail.com>
2019-04-23 14:01:39 +0530

    Tears of joy and confusion - Delay works now.
    Though no one could logically determine why the debounce fixed
    anything. How come it wasn't working without it?

--- src/code.asm
+++ src/code.asm
@@ -8,35 +8,33 @@
     
 .CODE
 .STARTUP
+
         ORG 0000h
         
-        ; In register storage of the number of people that entered the room,
-        MOV  dl, 00h                 
-                         
-        ; Set up the 8255 to read from ports A and B, and write to port C.
-        MOV  al, 10010000b
-        OUT  CWREG, al
+        ; This register will serve as memory and store the number of people in the room.
+        mov dl, 00h
         
-; Continuously read from port C to see if someone has stepped on the pressure
-; plates.
-start:  IN   al, PORTA
-        ; START DEBUG
-        SHR  al, 4
-        OUT  PORTB, al 
-        CMP  al, 00010000b
-        ; END DEBUG
-        JNE  start
-        CALL increment
-        MOV  al, dl
-        OUT  PORTC, al
-        JMP  start 
+        ; Set up the PPI to read from Port A and write to Port B and Port C.
+        mov al, 10010000b
+        OUT CWREG, al
+        
+main:   IN al, PORTA
+        CALL delay
+        OUT PORTB, al
+        CMP al, 00000001b
+        JNE main
+        INC dl
+        MOV al, dl
+        OUT PORTC, al
+        JMP main 
 
-PROC increment
-        INC  dl
-        CMP  dl,09h
-        JLE  inc1
-        MOV  dl, 00h
-inc1:   ret
-ENDP increment
+delay PROC
+        MOV CX, 000Fh
+p1:     NOP
+        DEC CX
+        CMP CX, 0000h
+        JNE p1
+        ret
+delay ENDP
 
-END
+END 
