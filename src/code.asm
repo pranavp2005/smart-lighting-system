69cca4062b91b32084b645a1a0f9bef5986c5618
Hemanth V. Alluri <hdrive1999@gmail.com>
2019-04-23 15:16:30 +0530

    Smart entry system almost complete.

--- src/code.asm
+++ src/code.asm
@@ -19,22 +19,35 @@
         OUT CWREG, al
         
 main:   IN al, PORTA
-        CALL delay
         OUT PORTB, al
         CMP al, 00000001b
         JNE main
-        INC dl
-        MOV al, dl
-        OUT PORTC, al
+        CALL delay_db   
+        CALL delay
+        CALL incr_cnt
         JMP main 
 
-delay PROC
-        MOV CX, 000Fh
+; A small timed delay
+delay PROC NEAR
+        MOV CX, 0FFFFh
 p1:     NOP
         DEC CX
-        CMP CX, 0000h
         JNE p1
         ret
 delay ENDP
+
+; A debounce delay mechanism implemented on Port A's buttons.
+delay_db PROC NEAR
+p2:     IN al, PORTA
+        CMP al, 0h
+        JNE p2
+        ret
+delay_db ENDP
+
+incr_cnt PROC NEAR
+        INC dl;
+        MOV al, dl
+        OUT PORTC, al
+incr_cnt ENDP
 
 END 
